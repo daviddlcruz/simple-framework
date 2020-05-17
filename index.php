c36565fecf2760b7444d92febce310fc53241dd6
daviddlcruz <55325956+daviddlcruz@users.noreply.github.com>
2020-05-17 10:58:08 -0500

    Introducing the HttpFoundation component

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+require_once __DIR__.'/init.php';
 
-$name = $_GET['name'];
+$name = $request->get('name', 'World');
 
-printf('Hello %s', $name);
+$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
